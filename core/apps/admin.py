07e738b438a4fc3bd6e37561c2531504af8ddebb
DiaaHussei <117247132+DiaaHussei@users.noreply.github.com>
2024-10-04 04:47:18 +0300

    fixed the models

--- core/apps/admin.py
+++ core/apps/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Category, StoreHouseType, StoreHouse
+from .models import Category, StoreHouseType, StoreHouse, Outgoing, Incoming, Station, Supplier, StoreHouseCategroy, Beneficiary
 
 # ModelAdmin for Category
 class CategoryAdmin(admin.ModelAdmin):
@@ -34,7 +34,7 @@ class StoreHouseTypeAdmin(admin.ModelAdmin):
 # ModelAdmin for StoreHouse
 class StoreHouseAdmin(admin.ModelAdmin):
     # Fields to display in the admin list view
-    list_display = ('name', 'store_categroy', 'storekeeper', 'phone_number', 'location', 'current_amount')
+    list_display = ('name', 'storekeeper', 'phone_number', 'location')
     
     # Fields to enable search functionality
     search_fields = ('name', 'storekeeper', 'phone_number', 'location')
@@ -43,16 +43,89 @@ class StoreHouseAdmin(admin.ModelAdmin):
     ordering = ('name',)
     
     # Option to filter by store category and location
-    list_filter = ('store_categroy', 'location')
+    # list_filter = ('location')
     
     # Custom method to display current_amount from related StoreHouseType
-    def current_amount(self, obj):
-        return obj.store_categroy.current_amount
+    # def current_amount(self, obj):
+    #     return obj.store_categroy
     
-    # Set a readable name for the custom column
-    current_amount.short_description = 'Current Amount'
+    # # Set a readable name for the custom column
+    # current_amount.short_description = 'Current Amount'
+    
+class StoreHouseCategroyAdmin(admin.ModelAdmin):
+    list_display = ('catergory', 'storehouse', 'measurement_type', 'opening_balance', 'current_amount')
+        
+    
+class IncomingAdmin(admin.ModelAdmin):
+    # Fields to display in the admin list view
+    list_display = ('incom_date', 'paper_number', 'supplier', 'station', 'supply_voucher_number', 'recipient_name', 'deliverer_name', 'imported_quantites', 'cat', 'note')
+    
+    # Fields to enable search functionality
+    search_fields = ('store_house__storehouse__name', 'paper_number', 'recipient_name', 'deliverer_name', 'imported_quantites', 'cat', 'note')
+    
+    # Ordering the list by incoming date
+    ordering = ('incom_date',)
+    
+    # Option to filter by store house, supplier, and station
+    list_filter = ('store', 'supplier', 'station')
+
+# ModelAdmin for Outgoing
+class OutgoingAdmin(admin.ModelAdmin):
+    # Fields to display in the admin list view
+    list_display = ('store_house', 'outging_date', 'paper_number', 'beneficiary', 'supply_voucher_number', 'recipient_name', 'deliverer_name', 'outgoing_quantites', 'cat', 'note', 'transfer_date', 'current_transfer_date')
+    
+    # Fields to enable search functionality
+    search_fields = ('store_house__name', 'paper_number', 'recipient_name', 'deliverer_name', 'outgoing_quantites', 'cat', 'note')
+    
+    # Ordering the list by outgoing date
+    ordering = ('outging_date',)
+    
+    # Option to filter by store house, beneficiary, and transfer dates
+    list_filter = ('store_house', 'beneficiary', 'transfer_date', 'current_transfer_date')
+
+
+
+class SupplierAdmin(admin.ModelAdmin):
+    # Fields to display in the admin list view
+    list_display = ('name', 'phone_number')
+    
+    # Fields to enable search functionality
+    search_fields = ('name', 'phone_number')
+    
+    # Ordering the list by supplier's name
+    ordering = ('name',)
+    
+    # Option to filter by name
+    list_filter = ('name',)
+
+# ModelAdmin for Station
+class StationAdmin(admin.ModelAdmin):
+    # Fields to display in the admin list view
+    list_display = ('station_name', 'location')
+    
+    # Fields to enable search functionality
+    search_fields = ('station_name', 'location')
+    
+    # Ordering the list by station name
+    ordering = ('station_name',)
+    
+    # Option to filter by station name and location
+    list_filter = ('station_name', 'location')
+
+
+class BeneficiaryAdmin(admin.ModelAdmin):
+    list_display = ('name', 'phone_number')
+    
+
+admin.site.register(Beneficiary, BeneficiaryAdmin)
+
+admin.site.register(Incoming, IncomingAdmin)
+admin.site.register(Outgoing, OutgoingAdmin)
+admin.site.register(StoreHouseCategroy, StoreHouseCategroyAdmin)
 
 # Registering all models with their respective ModelAdmin classes
 admin.site.register(Category, CategoryAdmin)
 admin.site.register(StoreHouseType, StoreHouseTypeAdmin)
 admin.site.register(StoreHouse, StoreHouseAdmin)
+admin.site.register(Supplier, SupplierAdmin)
+admin.site.register(Station, StationAdmin)
--- core/apps/models.py
+++ core/apps/models.py
@@ -3,6 +3,12 @@ from django.utils.translation import gettext_lazy as _
 from django.core.validators import RegexValidator
 from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin, BaseUserManager
 from django.utils import timezone
+from django.contrib.contenttypes.fields import GenericRelation
+from django.contrib.contenttypes.fields import GenericForeignKey
+from django.contrib.contenttypes.models import ContentType
+from django.core.exceptions import ValidationError
+
+
 
 
 
@@ -112,14 +118,11 @@ class User(AbstractBaseUser, PermissionsMixin):
     
 class Beneficiary(models.Model):
     name = models.CharField(_("Full Name"), max_length=50)
-    phone_number = models.CharField(max_length=9, validators=[
-            RegexValidator(
-                regex=r'^\+?1?\d{9,15}$',  # Example regex for international phone numbers
-                message="Phone number must be entered in the format: '+999999999'. Up to 15 digits allowed."
-            )
-        ])
+    phone_number = models.CharField(max_length=9)
     class Meta:
         db_table = 'Beneficiary'
+    def __str__(self):
+        return self.name
     
 class Station(models.Model):
     station_name = models.CharField(_("Station Name"), max_length=50)
@@ -127,6 +130,8 @@ class Station(models.Model):
     
     class Meta:
         db_table = 'Station'
+    def __str__(self):
+        return self.station_name
 
 
 class Category(models.Model):
@@ -162,7 +167,10 @@ class StoreHouseType(models.Model):
 
 class StoreHouse(models.Model):
     name = models.CharField(_("name"), max_length=50)
-    store_categroy = models.ForeignKey(StoreHouseType, verbose_name=_("StoreHouseType"), on_delete=models.CASCADE)
+    # store_categroy = models.ForeignKey(StoreHouseType, verbose_name=_("StoreHouseType"), on_delete=models.CASCADE)
+    # store_categroy = models.ManyToManyField(StoreHouseType)
+
+    
     storekeeper = models.CharField(_("Storekeeper"), max_length=100)
     phone_number = models.CharField(_("phone_number"), max_length=30)
     location = models.CharField(_("Location"), max_length=90)
@@ -175,15 +183,162 @@ class StoreHouse(models.Model):
 
     def __str__(self):
         return self.name
+    
+class StoreHouseCategroy(models.Model):
+    
+    catergory = models.ForeignKey(Category, verbose_name=_("category"), on_delete=models.CASCADE)
+    storehouse = models.ForeignKey(StoreHouse, verbose_name=_("storehouse"), on_delete=models.CASCADE)
+    measurement_type = models.CharField(_("measurement_type"), max_length=50)
+    opening_balance = models.FloatField(_("open_balance"))
+    current_amount = models.FloatField(_("amount"))
+    
+
+    class Meta:
+        verbose_name = _("StoreHouseCategroy")
+        verbose_name_plural = _("StoreHouseCategroy")
+        db_table = 'StoreHouseCategroy'
+        constraints = [
+            models.UniqueConstraint(fields=['catergory', 'storehouse'], name='unique_catergory_storehouse')
+        ]
+
+    def __str__(self):
+        return self.storehouse.name
+    
 
 class Supplier(models.Model):
     name = models.CharField(_("Full Name"), max_length=90, null=False)
-    phone_number = models.CharField(max_length=9, validators=[
-            RegexValidator(
-                regex=r'^\+?1?\d{9,15}$',  # Example regex for international phone numbers
-                message="Phone number must be entered in the format: '+999999999'. Up to 15 digits allowed."
-            )
-        ])
+    phone_number = models.CharField(max_length=9)
     class Meta:
         db_table = 'Supplier'
+        
+    def __str__(self):
+        return self.name
+
+# def generate_upload_to_path(instance, filename):
+#     if instance.content_type:
+#         return f'{instance.content_type.name}-images/{filename}'
+#     return f'images/unknown/{filename}'
+
+
+# class Image(models.Model):
+
+#     content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
+#     object_id = models.PositiveIntegerField()
+#     content_object = GenericForeignKey()
+#     image = models.ImageField(_("Image"), upload_to=generate_upload_to_path)
+#     # def save(self, *args, **kwargs):
+
+#     #     if self.content_type:
+#     #         self.image.upload_to = f'{self.content_type.name}/'
+#     #     return super().save(*args, **kwargs)
+#     def __str__(self):
+#         return self.image.url
+
+
+
+class Incoming(models.Model):
+    # store_house = models.ForeignKey(StoreHouse, verbose_name=_("StoreHouse"), on_delete=models.CASCADE)
+    store = models.ForeignKey(StoreHouseCategroy, verbose_name=_("StoreHouse"), on_delete=models.CASCADE)
+    incom_date = models.DateTimeField(_("incom_date"), auto_now=False, auto_now_add=False)
+    paper_number = models.CharField(_("Numbering on Paper"), max_length=50)
+    supplier = models.ForeignKey(Supplier, verbose_name=_("Supplier"), on_delete=models.CASCADE)
+    station = models.ForeignKey(Station, on_delete=models.CASCADE)
+    supply_voucher_number = models.CharField(max_length=50)
+    recipient_name = models.CharField(_("Recipient`s Name"), max_length=90)
+    deliverer_name = models.CharField(max_length=50)
+    recipient_miltry_number = models.CharField(max_length=50)
+    deliverer_miltry_number = models.CharField(max_length=50)
+    # statement = models.CharField(_("statement"), max_length=50)
+    # attach_file = GenericRelation(Image, related_query_name='attach_file')
+    imported_quantites = models.CharField(_("imported quantites"), max_length=50)
+    cat = models.CharField(_("catergory"), max_length=50)
+    note = models.CharField(_("note"), max_length=50)
+    
+    class Meta:
+        db_table = 'Incoming'
+        
+    def __str__(self):
+        return self.store.storehouse.name
+    
+    def save(self, *args, **kwargs):
+        # Convert imported_quantites to a numeric value
+        try:
+            imported_qty = float(self.imported_quantites)
+        except ValueError:
+            raise ValidationError(_("Imported quantities must be a valid number."))
+
+        # Update the current_amount in the related StoreHouseCategroy
+        store_categroy_qs = StoreHouseCategroy.objects.filter(catergory__name=self.cat)
+
+        # Update the current_amount for matching categories
+        for store_categroy in store_categroy_qs:
+            store_categroy.current_amount += imported_qty
+            store_categroy.save()
+
+        super().save(*args, **kwargs)
+
+    class Meta:
+        db_table = 'Incoming'
+    
+    
+    
+
+class Outgoing(models.Model):
+    store_house = models.ForeignKey(StoreHouse, verbose_name=_("StoreHouse"), on_delete=models.CASCADE)
+    outging_date = models.DateTimeField(_("outging date"), auto_now=False, auto_now_add=True)
+    paper_number = models.CharField(_("Numbering on Paper"), max_length=50)
+    beneficiary = models.ForeignKey(Beneficiary, verbose_name=_("Beneficiary"), on_delete=models.CASCADE)
+    supply_voucher_number = models.CharField(max_length=50)
+    recipient_name = models.CharField(_("Recipient`s Name"), max_length=90)
+    deliverer_name = models.CharField(max_length=50)
+    recipient_miltry_number = models.CharField(max_length=50)
+    deliverer_miltry_number = models.CharField(max_length=50)
+    # statement = models.CharField(_("statement"), max_length=50)
+    # attach_file = GenericRelation(Image, related_query_name='attach_file')
+    outgoing_quantites = models.CharField(_("outgoing quantites"), max_length=50)
+    cat = models.CharField(_("catergory"), max_length=50)
+    note = models.CharField(_("note"), max_length=50)
+    transfer_date = models.DateField(_("transfer date"), auto_now=False, auto_now_add=False)
+    current_transfer_date = models.DateField(_("transfer date"), auto_now=False, auto_now_add=True)
+    
+    class Meta:
+        db_table = 'Outgoing'
+        
+    def __str__(self):
+        return self.store_house.name
+    
+    
+    def save(self, *args, **kwargs):
+        outgogin_qty = float(self.outgoing_quantites)
+        storehouse_type = self.store_house.id
+        print(storehouse_type)
+        
+        store_categroy_qs = StoreHouseCategroy.objects.filter(storehouse=self.store_house, catergory__name=self.cat)
+        print(store_categroy_qs)
+        for stor in store_categroy_qs:
+            if stor.current_amount < outgogin_qty:
+                raise ValidationError(_("Not enough stock in the storehouse for this category."))  
+              
+            
+            stor.current_amount -= outgogin_qty
+            print(stor.current_amount)
+            stor.save()
+        
+        
+        # Update the current_amount of StoreHouseType
+        # storehouse_type.current_amount -= outgogin_qty
+        # storehouse_type.save()
+        
+        # Call the original save method to save the Incoming object
+        super().save(*args, **kwargs)
+
+
+
+ 
+    
+    
+      
+      
+    
+    
     
